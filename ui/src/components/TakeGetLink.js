139c8081d1ac715c1fee3194e60e8ad6d3d23ad2
anastybl <anasty_b@mail.ru>
2024-11-07 23:59:06 +0300

    Added input and button click handler

--- ui/src/components/TakeGetLink.js
+++ ui/src/components/TakeGetLink.js
@@ -10,22 +10,15 @@ export default class TakeGetLink extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-    
+
     handleChange(event) {
         this.setState({value: event.target.value});
     }
-    
+
     handleSubmit(event) {
-        console.log('here')
+        console.log(this.state.value);
+        event.preventDefault();
     }
-    
-    // addLink(link) {
-    //     if (link) {
-    //         var xhr = new XMLHttpRequest();
-    //         xhr.open('post', url, true);
-    //         xhr.send(link);
-    //     }
-    // }
 
     render() {
         return (
@@ -42,22 +35,22 @@ export default class TakeGetLink extends Component {
                 <h1
                     className="text-center"
                     style={{margin: '20px'}} > Укоротитель ссылок </h1>
-                <Form onSubmit={this.ha}>
+                <Form onSubmit={this.handleSubmit}>
                     <Form.Group style={{margin: '20px'}}>
                         <Form.Label> Помогите клиентам быстро найти вашу страницу в интернете. Благодаря короткой ссылке клиентам не придётся видеть длинные url-адреса, занимающие много места.</Form.Label>
                         <Form.Control
-                            type="link"
+                            type="text"
                             value={this.state.value}
                             onChange={this.handleChange}
                             placeholder="Введите ссылку, которую нужно сократить" />
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            style={{margin: '20px'}} > Сократить </Button>
                     </Form.Group>
-
-                    <Button
-                        variant="primary"
-                        type="submit"
-                        style={{margin: '20px'}} > Сократить </Button>
                 </Form>
             </Container>
         )
     }
 }
+
