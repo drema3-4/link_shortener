2bd58d44f2b0ec8cda776ba409a7ca287fe15f83
anastybl <anasty_b@mail.ru>
2024-11-08 01:36:14 +0300

    Added prototype auth front

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
 import Header from './components/Header.js';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './pages/Home.js';
+import Login from "./auth/Login.js";
+import Dashboard from "./auth/DashBoard.js";
+import AuthProvider from "./auth/AuthProvider.js";
+import PrivateRoute from "./auth/PrivateRoute.js";
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div className="App">
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+            {/* Other routes */}
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
